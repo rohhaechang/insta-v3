0a4a783adb848b763f616ca3530a4fb1c90e88dc
haechang <94176916+rohhaechang@users.noreply.github.com>
2022-11-30 15:51:23 +0900

    add the menu and profile image

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
-import {MagnifyingGlassIcon} from '@heroicons/react/24/outline'
+import {MagnifyingGlassIcon, PlusCircleIcon} from '@heroicons/react/24/outline'
+import {HomeIcon} from '@heroicons/react/24/solid'
 
 export default function Header() {
   return (
-    <div className='flex items-center justify-between max-w-6xl'>
+    <div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
       
       {/* Left */}
       
@@ -32,7 +33,16 @@ export default function Header() {
         
       {/* Right */}
 
-      <h1>Right sides</h1>
+      <div className='flex space-x-4 items-center'>
+        <HomeIcon className='hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
+        <PlusCircleIcon className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out' />
+        <Image 
+          src="https://w.namu.la/s/126588db9d20e18ebae80ad547e784ffc25c2e2a62d41e2e0d6bc0ec45bf77c4e0dc3f757cd2ea5e1690ace9288f424edb6f0c5bfd5da345f4b4c254e6d129603aae552d36278efb1382b484ef37d46bcf4552ff44eca993b8fcc745def00f6bea496d0b7bcd85c7506c2d17c62fda33"
+          alt='picture'
+          className='rounded-full object-contain cursor-pointer'
+          width={40}
+          height={40} />
+      </div>
     </div>
   )
 }
