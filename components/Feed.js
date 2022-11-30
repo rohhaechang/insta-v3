7402dfbaeff90c7165e8a8a5c75c06308e099b31
haechang <94176916+rohhaechang@users.noreply.github.com>
2022-11-30 19:35:26 +0900

    make feed section responsive

--- components/Feed.js
+++ components/Feed.js
@@ -4,8 +4,8 @@ import Posts from './Posts'
 
 export default function Feed() {
   return (
-    <main>
-      <section>
+    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
+      <section className="md:col-span-2">
         {/* Stories */}
 
         <Stories />
@@ -14,7 +14,7 @@ export default function Feed() {
 
         <Posts />
       </section>
-      <section>
+      <section className="hidden md:inline-grid">
         {/* Mini profile */}
 
         {/* Suggestion */}
@@ -22,3 +22,6 @@ export default function Feed() {
     </main>
   )
 }
+
+// md 사이즈 일때 grid-cols-3 인데, 스토리 섹션에서 2만큼을 가져간다고 한 것이다.(md:col-span-2)
+// 나머지 1은 mini profile 섹션에서 가져간다
