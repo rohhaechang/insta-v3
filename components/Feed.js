381dffdfb435ea2cc2c88e6eb24a2fd2e2dc804c
haechang <94176916+rohhaechang@users.noreply.github.com>
2022-11-30 16:20:57 +0900

    add fake data using minifaker and map through it inside at story component

---
+++ components/Feed.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Stories from './Stories'
+
+export default function Feed() {
+  return (
+    <main>
+      <section>
+      {/* Stories */}
+        <Stories />
+      {/* Posts */}
+      </section>
+      <section>
+      {/* Mini profile */}
+
+      {/* Suggestion */}
+      </section>
+    </main>
+  )
+}
--- components/Header.js
+++ components/Header.js
@@ -13,13 +13,15 @@ export default function Header() {
             <Image
               src="http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png"
               fill
-              className='object-contain '/>
+              className='object-contain '
+              alt='logo'/>
           </div>
           <div className='cursor-pointer h-24 w-10 relative lg:hidden'>
             <Image
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png"
               fill
-              className='object-contain '/>
+              className='object-contain '
+              alt='logo'/>
           </div>
 
         {/* Middle */}
